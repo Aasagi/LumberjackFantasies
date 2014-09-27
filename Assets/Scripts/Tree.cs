b196898c158399a69119292d85a16353e313e00e
Adam Politanski <cousken@gmail.com>
2014-09-27 12:50:50 +0200

    Fixed merge problems

--- Assets/Scripts/Tree.cs
+++ Assets/Scripts/Tree.cs
@@ -1,80 +1,85 @@
 ﻿using System;
+using Assets.Scripts;
 using UnityEngine;
+using System.Collections;
+using Object = UnityEngine.Object;
 
-namespace Assets.Scripts
+public class Tree : MonoBehaviour
 {
-    public class Tree : MonoBehaviour
+    public int Health;
+
+    private float cutCooldown;
+    public GameObject treeHitPrefab;
+    public GameObject treeDeathPrefab;
+    // Use this for initialization
+    void Start()
     {
-        public int Health;
+    }
 
-        private float cutCooldown;
-        public GameObject treeHitPrefab;
-        public GameObject treeDeathPrefab;
-        public GameObject logPickUp;
-        // Use this for initialization
-        void Start()
+    // Update is called once per frame
+    void Update()
+    {
+        if (cutCooldown > 0.0f)
         {
+            cutCooldown -= Time.deltaTime;
         }
-
-        // Update is called once per frame
-        void Update()
+        else
         {
-            if (cutCooldown > 0.0f)
+            if (Health <= 0)
             {
-                cutCooldown -= Time.deltaTime;
-            }
-            else
-            {
-                if (Health <= 0)
+                if (rigidbody.IsSleeping())
                 {
-                    if (rigidbody.IsSleeping())
-                    {
-                        Instantiate(treeDeathPrefab, transform.position, new Quaternion());
-                        Instantiate(logPickUp, transform.position, new Quaternion());
-                        Instantiate(logPickUp, transform.position, new Quaternion());
-                        Destroy(gameObject);
-                    }
+                    Instantiate(treeDeathPrefab, transform.position, new Quaternion());
+                    AddOnFunctions.KillAndDestroy(gameObject);
                 }
             }
         }
+    }
 
-        private void InflictDamage(int damage)
+    void FixedUpdate()
+    {
+        if (Health > 0 && rigidbody.transform.up.y < 0.8)
         {
-            Health = Math.Max(0, Health - damage);
-            Debug.Log("Health left: " + Health);
+            InflictDamage(Health);
+        }
+    }
+
+    private void InflictDamage(int damage)
+    {
+        Health = Math.Max(0, Health - damage);
+        Debug.Log("Health left: " + Health);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Cut(collider);
+    }
+
+    private void Cut(Collider collider)
+    {
+        if (Health <= 0 || cutCooldown > 0.0f)
+        {
+            return;
         }
 
-        void OnTriggerEnter(Collider collider)
+        InflictDamage(10);
+        var hitPosition = collider.transform.position;
+
+        cutCooldown = 0.2f;
+        Instantiate(treeHitPrefab, hitPosition, new Quaternion());
+        if (Health > 0)
         {
-            Cut(collider);
+            animation.Play("JunkWiggle");
         }
-
-        private void Cut(Collider collider)
+        else
         {
-            if (Health <= 0 || cutCooldown > 0.0f)
-            {
-                return;
-            }
-
-            InflictDamage(10);
-            var hitPosition = collider.transform.position;
-
-            cutCooldown = 0.2f;
-            Instantiate(treeHitPrefab, hitPosition, new Quaternion());
-            if (Health > 0)
-            {
-                //rigidbody.AddForce(new Vector3(0.0f, 100.0f, 0.0f));
-            }
-            else
-            {
-                Timber(hitPosition, transform.position - collider.transform.position);
-            }
+            Timber(hitPosition, transform.position - collider.transform.position);
         }
+    }
 
-        private void Timber(Vector3 hitPosition, Vector3 direction)
-        {
-            Instantiate(treeDeathPrefab, hitPosition, new Quaternion());
-            rigidbody.AddForce(new Vector3(500.0f * direction.x, 0.0f, 500.0f * direction.z));
-        }
+    private void Timber(Vector3 hitPosition, Vector3 direction)
+    {
+        Instantiate(treeDeathPrefab, hitPosition, new Quaternion());
+        rigidbody.AddForce(new Vector3(500.0f * direction.x, 0.0f, 500.0f * direction.z));
     }
 }
