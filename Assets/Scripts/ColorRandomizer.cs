33ee80d4fc09c6a5c0e3a4d7b2239680b127c983
Adam Politanski <cousken@gmail.com>
2014-09-27 21:24:09 +0200

    Failed color randomizing

---
+++ Assets/Scripts/ColorRandomizer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class ColorRandomizer : MonoBehaviour
+{
+
+    public List<Color> Colors;
+
+	// Use this for initialization
+    private void Start()
+    {
+        //renderer.material.color = Colors[Random.Range(0, Colors.Count)];
+    //    var color = Colors[Random.Range(0, Colors.Count)];
+    //    var material = new Material(renderer.material) {color = color};
+    //    renderer.material = material;
+    }
+
+    // Update is called once per frame
+	void Update () {
+	
+	}
+}
