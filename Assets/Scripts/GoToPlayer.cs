1f2a660a0d929cf129a76d92a94bfab8036ad745
Adam Politanski <cousken@gmail.com>
2014-09-27 22:45:24 +0200

    Pickups follow the player that spawned them Correct level up

---
+++ Assets/Scripts/GoToPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToPlayer : MonoBehaviour
+{
+
+    public Transform PlayerPosition;
+    public float SpeedMultiplayer;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (PlayerPosition == null) return;
+        
+        var position = transform.position;
+
+	    transform.position = Vector3.MoveTowards(position, PlayerPosition.position, Time.deltaTime * SpeedMultiplayer);
+	}
+}
